4412267929ade0c80e7a815b3e58ef3ac202fdb4
tmyersjstar <tmyers@jstar.ai>
2017-07-18 22:07:43 -0600

    run ws

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
 
 const PORT = 8080;
 
@@ -8,6 +10,16 @@ app.get('/', (req, res) => {
   res.send('Hello Tim\'s World!');
 });
 
-app.listen(PORT, () => {
-  console.log('Example app listening on port 3000!');
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log('received: %s', message);
+    ws.send(message);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log('Listening on %d', server.address().port);
 });
