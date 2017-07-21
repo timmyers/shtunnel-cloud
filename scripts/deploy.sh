618e6c7a7c8ca8d4268a7c87b508a73ab9951213
tmyersjstar <tmyers@jstar.ai>
2017-07-21 15:19:13 -0600

    Run scrip twith ssh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push timmyers/shtunnel-cloud
-ssh -o "StrictHostKeyChecking=no" root@tmye.me
+ssh -o "StrictHostKeyChecking=no" root@tmye.me 'bash -s' < scripts/deploy_restart.sh
