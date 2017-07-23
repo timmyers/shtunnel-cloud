006d4e57987027ce11d6a88c62570fc24156eead
tmyersjstar <tmyers@jstar.ai>
2017-07-23 14:18:07 -0600

    ws status

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const WebSocket = require('ws');
 
 const PORT = 8080;
 
+const wsConnections = [];
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('One more time.');
+  res.send(`${wsConnections.length} ws connections`);
 });
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
+  wsConnections.push(ws);
   ws.on('message', (message) => {
     console.log('received: %s', message);
     ws.send(message);
