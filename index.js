a6c66aff6e583c280de63a3e859f63371c1d9782
tmyersjstar <tmyers@jstar.ai>
2017-07-23 14:58:54 -0600

    fetch every 10s

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
+const fetch = require('node-fetch');
 
 const PORT = 8080;
 
@@ -26,3 +27,11 @@ wss.on('connection', (ws) => {
 server.listen(PORT, () => {
   console.log('Listening on %d', server.address().port);
 });
+
+setInterval(() => {
+  console.log('Interval!');
+
+  fetch('https://github.com/')
+    .then(res => res.text())
+    .then(body => console.log(body));
+}, 10000);
